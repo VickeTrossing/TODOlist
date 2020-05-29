c33eccefd150daa4b94b8d9a6feb450e08cc11e2
VickeTrossing <vitro001@gmail.com>
2020-05-29 10:18:37 +0200

    added alot of css and some javascript

---
+++ app.js
@@ -0,0 +1,26 @@
+    var firebaseConfig = {
+        apiKey: "AIzaSyB4X6PJnCSfVdqX55VSHFjj-BdsF5Dp7tk",
+        authDomain: "todolist-8ef9f.firebaseapp.com",
+        databaseURL: "https://todolist-8ef9f.firebaseio.com",
+        projectId: "todolist-8ef9f",
+        storageBucket: "todolist-8ef9f.appspot.com",
+        messagingSenderId: "104560995309",
+        appId: "1:104560995309:web:5ca90d64988663fb2195e4"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+
+const add_task = () => console.log("add_task");
+
+    const task_done = () => {
+        console.log("task_done");
+    }
+
+
+    const task_edit = () => console.log("task_edit");
+
+
+    const task_delete = () => {
+        console.log("task_delete");
+    }
